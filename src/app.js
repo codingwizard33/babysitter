68f734b44d99f004fcf5dbd30a42b6bf82d126fe
Your Name <you@example.com>
2023-10-18 17:40:44 +0400

    create server, user and role models, implements utils for check and create roles and admin user when application starting

--- src/app.js
+++ src/app.js
@@ -2,7 +2,16 @@ import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 
+import DBconnect from "./config/database.js";
+import { createRoles } from "./utils/createRoles.js";
+import { createUserAdmin } from "./utils/createUserAdmin.js";
+
 dotenv.config();
+
+DBconnect();
+
+await createRoles();
+await createUserAdmin();
 
 const app = express();
 app.use(express.json({ limit: '50mb' }));
--- src/models/User.js
+++ src/models/User.js
@@ -1,1 +1,55 @@
 import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true
+  },
+  email: {
+    unique: true,
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true
+  },
+  avatar: {
+    type: String,
+    required: false
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  location: {
+    longitude: {
+      type: Number,
+      required: true,
+    },
+    latitude: {
+      type: Number,
+      required: true
+    }
+  },
+  role: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Role'
+  }
+}, { timeseries: true });
+
+userSchema.pre('save', async function (next) {
+  const User = this.model('User');
+  const existingUser = await User.findOne({ email: this.email });
+  if (existingUser) {
+    const error = new Error('Email address already exists.');
+    return next(error);
+  }
+  next();
+});
+
+export default mongoose.model('User', userSchema);
