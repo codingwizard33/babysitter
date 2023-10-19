9d98226e095792009a5a07c9c019376ddb8f78dd
Your Name <you@example.com>
2023-10-19 10:26:05 +0400

    importing routes separating by files

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ import cors from "cors";
 import DBconnect from "./config/database.js";
 import { createRoles } from "./utils/createRoles.js";
 import { createUserAdmin } from "./utils/createUserAdmin.js";
+import web from "./routes/web.js";
+import auth from "./routes/auth.js";
+import api from "./routes/api.js";
 
 dotenv.config();
 
@@ -16,6 +19,10 @@ await createUserAdmin();
 const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(cors({ origin: '*' }));
+
+app.use('/', web);
+app.use('/auth', auth);
+app.use('/api', api);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server started on port ${process.env.PORT}.`);
