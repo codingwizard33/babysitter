da8f2827dce4b27b291d0f97e50fdcf9d528e89d
Your Name <you@example.com>
2023-10-20 14:21:21 +0400

    created admin user nanny middlewares

---
+++ src/middlewares/userMiddleware.js
@@ -0,0 +1,25 @@
+import { userResource } from "../resources/userResource.js";
+import { jwtVerificationService } from "../services/auth/jwtVerificationService.js";
+
+const userMiddleware = async (req, res, next) => {
+  try {
+    var token = req.header('Authorization').split(' ')[1];
+  } catch (error) {
+    return res.json({ message: 'Authentication failed.' });
+  }
+
+  try {
+    const userId = await jwtVerificationService(token);
+
+    const userRole = await userResource(userId.userId);
+
+    if (userRole.role !== 'user')
+      return res.status(403).json({ message: 'You do not have permission to access this resource.'});
+
+    next();
+  } catch (error) {
+    return res.json({ message: error.message });
+  }
+};
+
+export { userMiddleware };
