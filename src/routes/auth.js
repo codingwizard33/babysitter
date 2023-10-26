5bf0993856df86ecc510b0171bf662708ce40f99
Your Name <you@example.com>
2023-10-26 16:32:21 +0400

    routes bug fix

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,6 +5,7 @@ import { signUpController } from "../controllers/auth/signUpController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 import { logOutController } from "../controllers/auth/logOutController.js";
 import { refreshTokenController } from "../controllers/auth/refreshTokenController.js";
+import { emailVerificationController } from "../controllers/auth/emailVerificationController.js";
 
 const router = express.Router();
 
@@ -12,5 +13,6 @@ router.post('/sign-in', signInController);
 router.post('/sign-up', signUpController);
 router.post('/log-out', authMiddleware, logOutController);
 router.post('/refresh-token', authMiddleware, refreshTokenController);
+router.get('/email-verification/:token', emailVerificationController);
 
 export default router;
